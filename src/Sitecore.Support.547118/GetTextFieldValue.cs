11bf00629627a66e21d8d953fd3b32572acbc60e
DK\ands <ands@sitecore.net>
2022-10-24 11:28:14 +0300

    first

---
+++ src/Sitecore.Support.547118/GetTextFieldValue.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Configuration.KnownSettings;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.RenderField;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.RenderField
+{
+    public class GetTextFieldValue: Sitecore.Pipelines.RenderField.GetTextFieldValue
+    {
+        private static readonly BindingFlags _bflags = BindingFlags.NonPublic | BindingFlags.Static;
+        private static readonly PropertyInfo SettingsInstance_Prop = typeof(Sitecore.Configuration.Settings).GetProperty("SettingsInstance", _bflags);
+        private static readonly BaseSettings SettingsInstance = SettingsInstance_Prop.GetValue(null) as Sitecore.Abstractions.BaseSettings;
+
+        protected override void EncodeFieldValue([NotNull] RenderFieldArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (SettingsInstance.Rendering().HtmlEncodedFieldTypes.Contains(args.FieldTypeKey))
+            {
+                args.Result.FirstPart = HttpUtility.HtmlEncode(args.Result.FirstPart);
+            }
+        }
+    }
+}
